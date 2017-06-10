9b00f25c452d796347521f29f6c365ae0f3aed47
Radu Grigore <radugrigore@gmail.com>
2017-06-11 00:52:34 +0100

    part of the encoding

---
+++ src/qbf.ml
@@ -0,0 +1,40 @@
+open Printf
+
+type variable = string
+
+type t =
+  | Var of variable
+  | Not of t
+  | And of t list
+  | Or of t list
+  | All of variable * t
+  | Any of variable * t
+
+let mk_var v = Var v
+let mk_implies f g = Or [Not f; g]
+let mk_and xs = And xs
+
+let rec pp_list_sep sep pp_x f = function
+  | [] -> ()
+  | [x] -> pp_x f x
+  | x :: ((_ :: _) as xs) ->
+      fprintf f "%a%s%a" pp_x x sep (pp_list_sep sep pp_x) xs
+
+let pp_t f e =
+  let i = ref 0 in
+  let g () = incr i; (true, sprintf "g%d" !i) in
+  let neg (b, x) = (not b, x) in
+  let pp_var f (b, x) =
+    fprintf f "%s%s" (if b then "" else "-") x in
+  let rec go_op op es =
+    let vs = List.map go es in
+    let w = g () in
+    fprintf f "%a = %s(%a)\n" pp_var w op (pp_list_sep "," pp_var) vs;
+    w
+  and go = function
+    | Var x -> (true, x)
+    | Not e -> neg (go e)
+    | And es -> go_op "and" es
+    | Or es -> go_op "or" es
+    | _ -> failwith "todo" in
+  go e
--- src/wmmqbf.ml
+++ src/wmmqbf.ml
@@ -18,8 +18,57 @@ let parse fn =
             let msg = sprintf "@[%s:%d:%d: parse error@]" fn line (c1-c0+1) in
             raise (Parsing_failed msg))
 
+let mkV prefix bounds xs =
+  let chk (a, c) b = assert (a <= b && b <= c) in
+  List.iter2 chk bounds xs;
+  let name = Buffer.create 10 in
+  Printf.bprintf name "%s" prefix;
+  let app x = Printf.bprintf name "_%d" x in
+  List.iter app xs;
+  Qbf.mk_var (Buffer.contents name)
+
+let mkV1 prefix (l1, h1) x1 =
+  mkV prefix [(l1, h1)] [x1]
+let mkV2 prefix (l1, h1) (l2, h2) x1 x2 =
+  mkV prefix [(l1, h1); (l2, h2)] [x1; x2]
+let mkV3 prefix (l1, h1) (l2, h2) (l3, h3) x1 x2 x3 =
+  mkV prefix [(l1, h1); (l2, h2); (l3, h3)] [x1; x2; x3]
+
+let range i k =
+  let r = ref [] in
+  for j = k downto i do r := j :: !r done;
+  !r
+
+
 let do_one fn =
   let wmm = parse fn in
+  let n = wmm.Wmm.events in
+  let c = mkV2 "c" (0,n) (1,n) in
+  let d = mkV2 "d" (1,n) (1,n) in
+  let e = mkV2 "e" (1,n) (1,n) in
+  let f = mkV3 "f" (1,n) (0,n) (1,n) in
+  let g = mkV3 "g" (1,n) (0,n) (1,n) in
+  let justifies _ _ =
+    Qbf.mk_and [] in (* TODO *)
+  let implies x y = Qbf.mk_and @@
+    List.map (fun i -> Qbf.mk_implies (x i) (y i)) (range 1 n) in
+  let equal x y = Qbf.mk_and [implies x y; implies y x] in
+  let small_step x j = Qbf.mk_and
+    [ implies (x (j - 1)) (x j)
+    ; justifies (x (j - 1)) (x j)] in
+  let before alpha x omega = Qbf.mk_and @@
+    equal alpha (x 0)
+    :: equal (x n) omega
+    :: List.map (small_step x) (range 1 n) in
+  let step k = Qbf.mk_and @@
+    [ equal (c (k-1)) (f k 0)
+    ; equal (f k n) (d k)
+    ; Qbf.mk_implies
+      (before (c (k - 1)) (f k) (d k))
+      (before (d k) (g k) (e k)) ] (* TODO: add  Ek -> Ck *)
+  in
+  let q = Qbf.mk_and @@ List.map step (range 1 n) in
+  Qbf.pp_t stdout q;
   ()
 
 let () =
