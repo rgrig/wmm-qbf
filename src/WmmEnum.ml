de8766585156bac9acc307e49b0d9b27cd096974
Radu Grigore <radugrigore@gmail.com>
2017-06-25 10:56:43 +0100

    dot printing
    outputs a tree only, because the full graph tends to be huge

--- src/WmmEnum.ml
+++ src/WmmEnum.ml
@@ -30,12 +30,33 @@ let dump_dot fn g =
   close_out o
 *)
 
+let sname_of xs =
+  let b = Buffer.create 10 in
+  let xs = List.sort compare xs in
+  let rec loop = function
+    | [] -> ()
+    | x :: xs -> bprintf b ",%d" x; loop xs in
+  let f = function
+    | [] -> bprintf b "E"
+    | x :: xs -> bprintf b "%d" x; loop xs in
+  f xs;
+  Buffer.contents b
+
 let name_of es xs =
   let b = Buffer.create (es.E.events_number + 1) in
   for i = 1 to es.E.events_number do
     bprintf b (if List.mem i xs then "1" else "0")
   done;
   Buffer.contents b
+
+let dump_dot fn whys =
+  let o = open_out fn in
+  fprintf o "digraph x {\n";
+  let dump_arc (x, y) =
+    fprintf o "  \"%s\" -> \"%s\";\n" (sname_of x) (sname_of y) in
+  List.iter dump_arc whys;
+  fprintf o "}\n";
+  close_out o
 
 let step es fn now =
   let x = MM.fresh_configuration es in
@@ -45,23 +66,24 @@ let step es fn now =
   List.map (MM.set_of_model y) (Qbf.models fn q)
 
 let do_one fn =
-  (* XXX: reinstate dot dumping *)
   let es = U.parse fn in
   let fn = Filename.remove_extension fn in
+  let whys = ref [] in
   let seen = Hashtbl.create 101 in
+  let look x xs y = if not (Hashtbl.mem seen y) then begin
+    printf "exec: %a\n%!" (U.hp_list_sep " " U.hp_int) y;
+    Hashtbl.add seen y ();
+    U.option () (fun x -> whys := (x, y) :: !whys) x;
+    Que.push y xs
+  end else xs in
   let rec bfs xs = if xs <> Que.empty then begin
     let x, xs = Que.pop xs in
     let fnx = sprintf "%s-%s" fn (name_of es x) in
     let ys = step es fnx x in
-    let look xs y = if not (Hashtbl.mem seen y) then begin
-      printf "exec: %a\n%!" (U.hp_list_sep " " U.hp_int) y;
-      Hashtbl.add seen y ();
-      Que.push y xs
-    end else xs in
-    bfs (List.fold_left look xs ys)
+    bfs (List.fold_left (look (Some x)) xs ys)
   end in
-  Hashtbl.add seen [] ();
-  bfs (Que.push [] Que.empty)
+  bfs (look None Que.empty []);
+  dump_dot (sprintf "%s.dot" fn) !whys
 
 let () =
   Arg.parse [] do_one "WmmEnum <infiles>"
