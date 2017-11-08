45508e6198dc12e2434fc5fadcd7625e1069fe03
graymalkin <sjc80@kent.ac.uk>
2017-11-08 11:52:42 +0000

    Infrastructure for running CAT parser. Needs tokeniser

--- test/CatTest.ml
+++ test/CatTest.ml
@@ -1,4 +1,37 @@
+open Printf
 module T = ModelParser
 
+exception Parsing_failed of string
+             
+let use_stdin = ref false
+            
+let parse filename f =
+  let lexbuf = Lexing.from_channel f in
+  try
+    let r = T.main (fun f -> T.EOF) lexbuf in
+    close_in_noerr f;
+    r
+  with
+    | ModelParser.Error ->
+        (match Lexing.lexeme_start_p lexbuf with
+        { Lexing.pos_lnum=line; Lexing.pos_bol=c0;
+          Lexing.pos_fname=_; Lexing.pos_cnum=c1} ->
+            let msg = sprintf "%s:%d:%d: parse error" filename line (c1-c0+1) in
+            raise (Parsing_failed msg))
+
+let run filename =
+    let file_chan = match !use_stdin with
+      true -> stdin
+    | false -> open_in filename
+  in
+  let _ = parse filename file_chan in
+  ()
+
+let cmd_spec =
+  Arg.align [
+      "-",  Arg.Unit (fun x -> use_stdin:= true; (run "stdin")), "  read from stdin"
+  ]
+
 let () =
-  Printf.printf "go\n"
+  Arg.parse cmd_spec run (sprintf "%s <infiles>" (Sys.executable_name))
+
