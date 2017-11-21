f9962693a7577e279d0afff7cda5016724f4a286
Sarah Harris <>
2017-11-21 11:43:16 +0000

    Slightly tidier instruction dump

--- herd-parsers-dirty/src/Hack.ml
+++ herd-parsers-dirty/src/Hack.ml
@@ -28,36 +28,22 @@ module LISA_LEXER = BellLexer.Make(LexUtils.Default)
 let lexbuf = U.from_section program_range (open_in filename)
 let titles, instructions, misc = LISAParser.main LISA_LEXER.token lexbuf
 
+(* Transpose rows of instructions into columns of instructions. *)
+let processes = Misc.transpose instructions
+
 (* Parse condition. *)
 let _ = print_range condition_range
 let lexbuf = U.from_section condition_range (open_in filename)
 let condition = StateParser.constraints STATE_LEXER.token lexbuf
 
-(* TODO
-(*
-  Transpose the instructions:
-  a list of rows -> a list of columns (each being the program
-  for a given processor
-*)
-    let transpose procs prog =
-      try
-	let prog = Misc.transpose prog in
-	List.combine procs prog 
-      with
-      |  Misc.TransposeFailure | Invalid_argument _ ->
-	  Warn.fatal "mismatch in instruction lines"
-*)
-
 (* Print inital state. *)
 let _ = Format.printf "Init: %a\n" MiscParser.pp_state init
 
 (* Print the program AST. *)
-let _ = List.iteri (fun line instructions ->
-  let by_process = List.combine titles instructions in
-  List.iter (fun (process, instruction) ->
-    Format.printf "Process %d: %a\n" process BellBase.pp_parsedPseudo instruction
-  ) by_process
-) instructions
+let _ = List.iter (fun (title, instructions) ->
+  Format.printf "Process %d:\n" title;
+  List.iter (fun instruction -> Format.printf "\t%a\n" BellBase.pp_parsedPseudo instruction) instructions
+) (List.combine titles processes)
 
 (* Print condition. *)
 let pp_locations (f : Format.formatter) (locations : (MiscParser.location * MiscParser.run_type) list) : unit =
