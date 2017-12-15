7075ffe06447ac735739ee6e4abc9ee08831ee92
Sarah Harris <>
2017-12-15 10:44:01 +0000

    Fixes and extra documentation after readthrought of LISA to event structure translation

--- src/LISAtoES/Translate.ml
+++ src/LISAtoES/Translate.ml
@@ -1,3 +1,5 @@
+(* This module translates LISA program ASTs from LISAParser into event structures. *)
+
 (* TODO: Replace tabs with spaces. *)
 (* TODO: Sanity check. *)
 
@@ -14,18 +16,21 @@ type address = {
 	offset : int;
 }
 
+(* Information about a read event, used as intermediate storage until justification has been calculated. *)
 type read {
 	id : event;
 	from : address;
 	value : int;
 }
 
+(* Information about a write event, used as intermediate storage until justification has been calculated. *)
 type write {
 	id : event;
 	to : address;
 	value : int;
 }
 
+(* Intermediate event structure, gets translated to the final datatype before leaving this module. *)
 type events {
 	reads : read list;
 	writes : write list;
@@ -40,47 +45,71 @@ let empty_events = {
 	order = [];
 }
 
+(* Translate a program AST into an event structure, this is the entrypoint into the module. *)
+(* `init` gives the initial values for global variables, letting the init event justify non-zero reads. *)
+(* `program` gives the multi-threaded program AST from LISAParser. *)
+(* `values` gives the range of numeric values to enumerate for read events. *)
 let translate
-	(init : LISAParser.TODO)
+	(init : LISAParser.MiscParser.state)
 	(program : LISAParser.BellBase.parsedInstruction list list)
 	(values : values)
 : EventStructure.t =
-	let store = (* TODO: From init. *)
+	(* The init event is special, and always gets the first ID number. *)
 	let init_id = 1;
+
+	(* This boxed counter is used to make sure all events get unique ID numbers. *)
 	let next_id = ref init_id + 1;
+
+	(* An initial set of write records needs to be added because the init event sets global to values *)
+	(* given in the initial state AST. *)
+	let events = writes_from_init init in
+
+	(* Translate each thread and compose the resulting event structures together. *)
 	let events = List.fold_left (fun events instructions ->
 		let instructions = Array.of_list instructions in
-		let subtree = translate_instructions instructions 0 store values next_id in
+		let subtree = translate_instructions instructions 0 Store.empty values next_id in
 		product events subtree
 	) empty_events program in
+
+	(* Add the order relations for the init event. *)
 	let events = prefix_event events init_id in
+
+	(* Generate the justifies relation. *)
+	(* In this case, "justifies" pairs reads and writes of the same variable and value. *)
 	let justifies = List.fold_left (fun accumulator read ->
-		List.fold_left (fun write ->
+		List.fold_left (fun accumulator write ->
 			if write_justifies_read write read then (write.id, read.id) :: accumulator else accumulator
 		) accumulator events.writes
 	) [] events.reads in
+
+	(* Convert from intermediate representation to final event structure. *)
 	{
 		events_number = 1 + (List.len events.reads) + (List.len events.writes);
 		reads = List.map (fun read -> read.id) events.reads;
-		justifies = TODO;
+		justifies = justifies;
 		conflicts = events.conflict;
 		order = events.order;
 	}
 
-(* TODO: Sanity check. *)
+(* Translates a sequence of instructions form a single thread into an event structure. *)
+(* `program_counter` gives the index of the instruction to interpret, allowing arbitrary branching. *)
+(* `depth` tracks the number of instructions interpreted to detect infinite loops. *)
 let rec translate_instructions
 	(instructions : LISAParser.BellBase.parsedInstruction array)
-	(program counter : int)
+	(program_counter : int)
 	(store : Store.t)
 	(values : values)
 	(next_id : event ref)
+	(depth : int)
 : events
 	if program_counter >= Array.len instructions then
+		(* Program finished. *)
 		{ reads=[]; writes=[]; conflict=[]; order=[]; }
 	else
 		let line = Array.get instructions program_counter in
+		let depth = depth + 1 in
 		match line with
-		| Nop -> translate_instructions instructions (program_counter + 1) store values next_id
+		| Nop -> translate_instructions instructions (program_counter + 1) store values next_id depth
 		| Label(_, instruction)
 		| Instruction instruction -> translate_instruction
 			instructions
@@ -88,47 +117,84 @@ let rec translate_instructions
 			store
 			values
 			next_id
+			depth
 			instruction
 		| Macro _
 		| Symbolic _ -> assert false
 
+(* Returns the event structure for an instruction and all following instructions. *)
+(* This awkward recursion pattern is here because branch instructions can arbitrarily change the program *)
+(* counter, and nesting instruction decoding pattern matches would be very unpleasant. *)
 and translate_instruction
 	(instructions : LISAParser.BellBase.parsedInstruction array)
 	(program counter : int)
 	(store : Store.t)
 	(values : values)
 	(next_id : event ref)
+	(depth : int)
 	(instruction : LISAParser.BellBase.parsedInstruction)
 : events
 	match instruction with
 	| Pld(destination, source, labels) ->
+		(* Spawn a set of conflicting read events, one for each value that could be read. *)
 		let destination = unwrap_reg destination in
 		let source = address_from_addr_op store source in
 		let program_counter = program_counter + 1 in
 		let events = ref empty_events in
 		for value = values.minimum to values.maximum do
 			let store = Store.update store destination value in
-			let subtree = translate_instructions instructions program_counter store values next_id in
+			let subtree = translate_instructions instructions program_counter store values next_id depth in
 			let subtree = prefix_read subtree next_id source value in
 			events := sum events subtree
 		done
 		events
 	| Pst(destination, source, labels) ->
+		(* Spawn a write event. *)
 		let destination = address_from_addr_op store destination in
 		let value = value_from_reg_or_imm store source in
 		let program_counter = program_counter + 1 in
-		let subtree = translate_instructions instructions program_counter store values next_id in
+		let subtree = translate_instructions instructions program_counter store values next_id depth in
 		prefix_write subtree next_id destination value
 	| Pbranch(Some(test), destination, labels) ->
+		(* Conditional jump, doesn't create any events directly. *)
 		let test = unwrap_reg test in
 		let value = Store.lookup store test in
 		(* TODO: What means true? *)
 		let next = if value != 0 then find_label instructions destination else program_counter + 1 in
-		translate_instructions instructions next store values next_id
+		translate_instructions instructions next store values next_id depth
 	| Pbranch(None, destination, labels) ->
+		(* Unconditional jump, doesn't create any events directly. *)
 		let next = find_label instructions destination in
-		translate_instructions instructions next store values next_id
+		translate_instructions instructions next store values next_id depth
 	| _ -> assert false (* TODO: Other instructions. *)
+
+(* Returns an event set with writes that attribute all the values in `init` to `event_id`. *)
+let writes_from_init (init : LISAParser.MiscParser.state) (event_id : event) : events =
+	let writes = List.fold_left (fun accumulator init ->
+		(* TODO: Assumes that run_type isn't relevant. *)
+		let (location, (run_type, value)) = init in
+
+		let value = match value with
+		| Concrete value -> value
+		| Symbolic _ -> assert false (* Meaningless. *)
+
+		let address = match location with
+		| Location_reg _
+		| Location_sreg _ -> assert false (* TODO: Allow writes to registers? *)
+		| Location_global(Symbolic name) -> { global = name; offset = 0; }
+		(* TODO: It doesn't look like the parser generates this in init. *)
+		(* TODO: This means that some of the JCTC LISA tests are broken because they use arrays in init. *)
+		| Location_deref(Symbolic name, offset) -> (* { global = name; offset = offset; } *) assert false
+		| Location_global(Concrete _)
+		| Location_deref(Concrete _, _) -> assert false (* Meaningless. *)
+		{ id = event_id; to = address; value = value; } :: accumulator
+	) [] init in
+	{
+		reads = [];
+		writes = writes;
+		conflict = [];
+		order = [];
+	}
 
 (* Return true if a write has the same address and value as a read. *)
 let write_justifies_read (write : write) (read : read) : bool =
@@ -145,7 +211,7 @@ let prefix_read (events : events) (next_id : event ref) (from : address) (value 
 		writes = events.writes;
 		conflict = events.conflict;
 		order = events.order;
-	} id in
+	} id
 
 (* Add a write event before the given events. *)
 let prefix_write (events : events) (next_id : event ref) (to : address) (value : int) : events =
@@ -156,11 +222,13 @@ let prefix_write (events : events) (next_id : event ref) (to : address) (value :
 		writes = { id = id; to = to; value = value; } :: events.writes;
 		conflict = events.conflict;
 		order = events.order;
-	} id in
+	} id
 
-(* Add an events before the given events, without updating lists of reads and writes. *)
-(* TODO: Avoid generating lots of unused conflicts. *)
+(* Add an event before the given event set, without updating lists of reads and writes. *)
+(* TODO: Avoid generating lots of unused orders. *)
 let prefix_event (events : events) (event : event) : events =
+	(* TODO: Check I understood the notation properly. *)
+	(* This should be equivalent to "≤0 ∪ ({⊥} × E)" but without (⊥, ⊥). *)
 	let read_order = List.map (fun read -> (event, read.id)) events.reads in
 	let write_order = List.map (fun write -> (event, write.id)) events.writes in
 	{
@@ -175,9 +243,12 @@ let prefix_event (events : events) (event : event) : events =
 (* Assumes that the two trees use separate sets of event IDs. *)
 let sum (a : events) (b : events) : events =
 	{
+		(* "E1 ∪ E2". *)
 		reads = a.reads @ b.reads;
 		writes = a.writes @ b.writes;
+		(* "#1 ∪ #2 ∪ (E1 × E2) ∪ (E2 × E1)", except the redundant conflicts are elided, see above. *)
 		conflict = (root_a, root_b) :: (root_b, root_a) :: (a.conflict @ b.conflict);
+		(* "≤1 ∪ ≤2". *)
 		order: a.order @ b.order;
 	}
 
@@ -185,23 +256,26 @@ let sum (a : events) (b : events) : events =
 (* Assumes that the two trees use separate sets of event IDs. *)
 let product (a : events) (b : events) : events =
 	{
+		(* "E1 ∪ E2". *)
 		reads = a.reads @ b.reads;
 		writes = a.writes @ b.writes;
+		(* "#1 ∪ #2". *)
 		conflict = a.conflict @ b.conflict;
+		(* "≤1 ∪ ≤2". *)
 		order: a.order @ b.order;
 	}
 
 (* Return the index of the first instruction with the given label. *)
 let find_label (instructions : LISAParser.BellBase.parsedInstruction array) (label : string) : int =
-	let rec search (instructions : instructions) (index : int) : int =
-		let index = index + 1 in
+	let rec search (instructions : LISAParser.BellBase.parsedInstruction array) (index : int) : int =
 		match Array.get instructions index with
-		| Nop -> search instructions index
 		| Label(test, _) where test = label -> index
-		| Label _ -> search instructions index
-		| Instruction _ -> search instructions index
-		| Macro _ -> assert false
+		| Nop
+		| Label _
+		| Instruction _ -> search instructions (index + 1)
+		| Macro _
 		| Symbolic _ -> assert false
+	(* Search until an index out of range happends. *)
 	try search instructions 0
 	with Invalid_argument _ -> raise Invalid_argument "label not found: " ^ label
 
@@ -213,8 +287,8 @@ let address_from_addr_op
 	| Addr_op_atom(reg_or_addr) -> address_from_reg_or_addr reg_or_addr
 	| Addr_op_add(reg_or_addr, reg_or_imm) ->
 		let base = address_from_reg_or_addr reg_or_addr in
-		let offset = value_from_reg_or_imm reg_or_imm in
-		{ global=base.global; offset=base.offset + offset; }
+		let offset = value_from_reg_or_imm store reg_or_imm in
+		{ global = base.global; offset = base.offset + offset; }
 
 let address_from_reg_or_addr
 	(source : LISAParser.BellBase.reg_or_addr)
@@ -222,7 +296,7 @@ let address_from_reg_or_addr
 	match source with
 	| Rega register -> assert false (* TODO: What does this mean? *)
 	| Abs(Concrete _) -> assert false (* Not allowed by parser. *)
-	| Abs(Symbolic global)) -> { global=global; offset=0; }
+	| Abs(Symbolic global)) -> { global = global; offset = 0; }
 
 let value_from_reg_or_imm
 	(store : Store.t)
