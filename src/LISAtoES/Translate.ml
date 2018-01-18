60821a029715839031bf1d10dc7aa4d358bdb90e
Sarah Harris <>
2018-01-18 17:00:34 +0000

    Replaced dummy writes with set of sets of events that tag branches where the condition is met

--- src/LISAtoES/Translate.ml
+++ src/LISAtoES/Translate.ml
@@ -1,9 +1,20 @@
 (* This module translates LISA program ASTs from LISAParser into event structures. *)
 
+(* TODO
+ * + Parse condition into map thread -> (register * value) list
+ * + Make prefix event always take id
+ * + Track parent event
+ * + Check registers match at branch end and return list of accepted end events
+ * + Merge list of events when spawning branches
+ * + Generate set of sets when composing final tree
+ *)
+
 open MiscParser
 open Constant
 open BellBase
 open MetaConst
+
+module ThreadMap = Util.IntMap
 
 exception LISAtoESException of string
 
@@ -154,11 +165,14 @@ let product (a : events) (b : events) : events =
     order =  a.order @ b.order;
   }
 
+let get_id (next_id : EventStructure.event ref) : EventStructure.event =
+  let id = !next_id in
+  next_id := id + 1;
+  id
+
 (* Add an event before the given event set, without updating lists of reads and writes. *)
 (* TODO: Avoid generating lots of unused orders. *)
 let prefix_event (events : events) (event : EventStructure.event) : events =
-  (* TODO: Check I understood the notation properly. *)
-  (* This should be equivalent to "≤0 ∪ ({⊥} × E)" but without (⊥, ⊥). *)
   let read_order = List.map (fun read -> (event, read.r_id)) events.reads in
   let write_order = List.map (fun write -> (event, write.w_id)) events.writes in
   {
@@ -168,10 +182,13 @@ let prefix_event (events : events) (event : EventStructure.event) : events =
     order = events.order @ read_order @ write_order;
   }
 
-(* Add a read event before the given events and return the result and the allocated event id. *)
-let prefix_read (events : events) (next_id : EventStructure.event ref) (r_from : address) (r_value : int) : events * EventStructure.event =
-  let r_id = !next_id in
-  next_id := !next_id + 1;
+(* Add a read event before the given events. *)
+let prefix_read
+  (events : events)
+  (r_id : EventStructure.event)
+  (r_from : address)
+  (r_value : int)
+: events =
   let events = prefix_event events r_id in
   let events = {
     reads = { r_id; r_from; r_value } :: events.reads;
@@ -179,18 +196,16 @@ let prefix_read (events : events) (next_id : EventStructure.event ref) (r_from :
     conflict = events.conflict;
     order = events.order;
   } in
-  (events, r_id)
+  events
 
 (* Add a write event before the given events. *)
 let prefix_write
   (events : events)
-  (next_id : EventStructure.event ref)
+  (w_id : EventStructure.event)
   (w_from : (int * int) option)
   (w_into : address)
   (w_value : int)
 : events =
-  let w_id = !next_id in
-  next_id := !next_id + 1;
   let events = prefix_event events w_id in
   {
     reads = events.reads;
@@ -223,7 +238,7 @@ let find_label (instructions : BellBase.parsedPseudo array) (label : string) : i
 let write_justifies_read (write : write) (read : read) : bool =
   read.r_from = write.w_into && read.r_value = write.w_value
 
-(* Returns a list of writes justified by the init event specified. *)
+(* Returns a list of writes justified by the init event whose id is given. *)
 let writes_from_init (init : MiscParser.state) (w_id : EventStructure.event) : write list =
   List.fold_left (fun accumulator init ->
     (* TODO: Assumes that run_type isn't relevant. *)
@@ -246,18 +261,31 @@ let writes_from_init (init : MiscParser.state) (w_id : EventStructure.event) : w
     { w_id; w_into; w_from = None; w_value } :: accumulator
   ) [] init
 
-(* Translates a sequence of instructions form a single thread into an event structure. *)
+(* Returns true if the state of all registers in the thread match the values in the condition. *)
+let condition_met (store : Store.t) (condition : (int * int) list) : bool =
+  let must_match = fun (register, value) ->
+    if (Store.lookup store register) != value then raise Not_found
+  in
+  try List.iter must_match condition; true
+  with Not_found -> false
+
+(* Translates a sequence of instructions from a single thread into an event structure. *)
+(* Returns event information and a set of events that tag branches where the final condition was met. *)
+(* `condition` gives a list of the expected values for each register in this thread. *)
 (* `program_counter` gives the index of the instruction to interpret, allowing arbitrary branching. *)
 (* `depth` tracks the number of instructions interpreted to detect infinite loops. *)
+(* `parent` gives the id of the parent event of this branch. *)
 let rec translate_instructions
   (thread_name : int)
   (instructions : BellBase.parsedPseudo array)
+  (condition : (int * int) list)
   (program_counter : int)
   (store : Store.t)
   (values : values)
   (next_id : EventStructure.event ref)
+  (parent : EventStructure.event)
   (depth : int)
-: events =
+: events * EventStructure.set =
   if depth > 16 then
     raise (LISAtoESException (Printf.sprintf
       "Program still running after %d instructions, aborting (maybe this program loops?)"
@@ -265,50 +293,74 @@ let rec translate_instructions
     ))
   else if program_counter >= Array.length instructions then
     (* Program finished. *)
-    { reads=[]; writes=[]; conflict=[]; order=[]; }
+    let accept = if condition_met store condition then [parent] else [] in
+    { reads=[]; writes=[]; conflict=[]; order=[]; }, accept
   else
     let line = Array.get instructions program_counter in
     let depth = depth + 1 in
     match line with
-    | Nop -> translate_instructions thread_name instructions (program_counter + 1) store values next_id depth
+    | Nop -> translate_instructions
+      thread_name
+      instructions
+      condition
+      (program_counter + 1)
+      store
+      values
+      next_id
+      parent
+      depth
     | Label(_, next_label) ->
       let instruction = instruction_from_pseudo_label next_label in
       (match instruction with
       | Some instruction -> translate_instruction
         thread_name
         instructions
+        condition
         program_counter
         store
         values
         next_id
+        parent
         depth
         instruction
-      | None -> translate_instructions thread_name instructions (program_counter + 1) store values next_id depth)
+      | None -> translate_instructions
+        thread_name
+        instructions
+        condition
+        (program_counter + 1)
+        store
+        values
+        next_id
+        parent
+        depth)
     | Instruction instruction -> translate_instruction
       thread_name
       instructions
+      condition
       program_counter
       store
       values
       next_id
+      parent
       depth
       instruction
     | Macro _
     | Symbolic _ -> assert false
 
-(* Returns the event structure for an instruction and all following instructions. *)
 (* This awkward recursion pattern is here because branch instructions can arbitrarily change the program *)
 (* counter, and nesting instruction decoding pattern matches would be very unpleasant. *)
 and translate_instruction
   (thread_name : int)
   (instructions : BellBase.parsedPseudo array)
+  (condition : (int * int) list)
   (program_counter : int)
   (store : Store.t)
   (values : values)
   (next_id : EventStructure.event ref)
+  (parent : EventStructure.event)
   (depth : int)
   (instruction : BellBase.parsedInstruction)
-: events =
+: events * EventStructure.set =
   match instruction with
   | Pld(destination, source, labels) ->
     (* Spawn a set of conflicting read events, one for each value that could be read. *)
@@ -316,6 +368,7 @@ and translate_instruction
     let source = address_from_addr_op store source in
     let program_counter = program_counter + 1 in
     let events = ref empty_events in
+    let accept = ref [] in
     let last_root = ref None in
 
     if !debug then
@@ -323,22 +376,26 @@ and translate_instruction
 
     for value = values.minimum to values.maximum do
       let new_store = Store.update store destination value in
-      let subtree = translate_instructions
+      let read_id = get_id next_id in
+      let subtree, subaccept = translate_instructions
         thread_name
         instructions
+        condition
         program_counter
         new_store
         values
         next_id
+        read_id
         depth
       in
-      let subtree, read_id = prefix_read subtree next_id source value in
+      let subtree = prefix_read subtree read_id source value in
+      accept := !accept @ subaccept;
       match !last_root with
       | Some last_id -> (events := sum !events subtree last_id read_id)
       | None -> (events := subtree);
       last_root := Some read_id
     done;
-    !events
+    !events, !accept
   | Pst(Addr_op_atom(Rega destination), source, labels) ->
     (* Special case for writing to a register, doesn't create any events directly. *)
     let destination = unwrap_reg destination in
@@ -349,7 +406,16 @@ and translate_instruction
     if !debug then
       Printf.printf "Register write r%d = %d\n" destination value;
 
-    translate_instructions thread_name instructions program_counter store values next_id depth
+    translate_instructions
+      thread_name
+      instructions
+      condition
+      program_counter
+      store
+      values
+      next_id
+      parent
+      depth
   | Pst(destination, source, labels) ->
     (* Spawn a write event. *)
     let destination = address_from_addr_op store destination in
@@ -363,8 +429,20 @@ and translate_instruction
     if !debug then
       Printf.printf "Store %s[%d] = %d\n" destination.global destination.offset value;
 
-    let subtree = translate_instructions thread_name instructions program_counter store values next_id depth in
-    prefix_write subtree next_id source_register destination value
+    let write_id = get_id next_id in
+    let subtree, accept = translate_instructions
+      thread_name
+      instructions
+      condition
+      program_counter
+      store
+      values
+      next_id
+      write_id
+      depth
+    in
+    let events = prefix_write subtree write_id source_register destination value in
+    events, accept
   | Pbranch(Some(test), destination, labels) ->
     (* Conditional jump, doesn't create any events directly. *)
     let test = unwrap_reg test in
@@ -375,14 +453,14 @@ and translate_instruction
 
     (* TODO: Check this definition of true is correct for LISA. *)
     let next = if value != 0 then find_label instructions destination else program_counter + 1 in
-    translate_instructions thread_name instructions next store values next_id depth
+    translate_instructions thread_name instructions condition next store values next_id parent depth
   | Pbranch(None, destination, labels) ->
     if !debug then
       Printf.printf "Jump %s\n" destination;
 
     (* Unconditional jump, doesn't create any events directly. *)
     let next = find_label instructions destination in
-    translate_instructions thread_name instructions next store values next_id depth
+    translate_instructions thread_name instructions condition next store values next_id parent depth
   | Pmov(destination, (RAI source)) ->
     (* Move from register or immediate. *)
     (* TODO: Support globals as source addresses, major restructuring needed. *)
@@ -394,7 +472,16 @@ and translate_instruction
     if !debug then
       Printf.printf "Mov r%d = %d\n" destination value;
 
-    translate_instructions thread_name instructions program_counter store values next_id depth
+    translate_instructions
+      thread_name
+      instructions
+      condition
+      program_counter
+      store
+      values
+      next_id
+      parent
+      depth
   | Pmov(destination, OP(operation, a, b)) ->
     (* Arithmetic, doesn't generate events. *)
     (* TODO: Support globals as source addresses, major restructuring needed. *)
@@ -408,7 +495,16 @@ and translate_instruction
     if !debug then
       Printf.printf "Arithmetic r%d = %d\n" destination value;
 
-    translate_instructions thread_name instructions program_counter store values next_id depth
+    translate_instructions
+      thread_name
+      instructions
+      condition
+      program_counter
+      store
+      values
+      next_id
+      parent
+      depth
   | _ -> assert false (* TODO: Other instructions. *)
 
 (* Generate the justifies relation. *)
@@ -435,53 +531,28 @@ let match_locations (reads : read list) (writes : write list) : EventStructure.r
     ) accumulator event_addresses
   ) [] event_addresses
 
-(* TODO: Move? *)
-(* Pretty much straight from the documentation. *)
-module IntPairs =
-  struct
-    type t = int * int
-      let compare (x0,y0) (x1,y1) =
-        match Pervasives.compare x0 x1 with
-        | 0 -> Pervasives.compare y0 y1
-        | c -> c
-  end
-module IntPairsMap = Map.Make(IntPairs)
-(* Aliases to help readability. *)
-module RegisterMap = IntPairsMap
-module ThreadMap = Util.IntMap
-
 (* Parse a string register name into a register number. *)
 let parse_register (name : string) : int =
   match BellBase.parse_reg name with
   | Some (BellBase.GPRreg i) -> i
   | _ -> raise (LISAtoESException "Failed to parse register name")
 
-(* Translate conjunctions (only) into maps. *)
 let rec parse_condition_expression
-  ((values, registers) : int RegisterMap.t * int list ThreadMap.t)
+  (accumulator : (int * int) list ThreadMap.t)
   (expression : MiscParser.prop)
-: int RegisterMap.t * int list ThreadMap.t = match expression with
-  | ConstrGen.And expressions -> List.fold_left parse_condition_expression (values, registers) expressions
+: (int * int) list ThreadMap.t = match expression with
+  | ConstrGen.And expressions -> List.fold_left parse_condition_expression accumulator expressions
   | ConstrGen.Atom (ConstrGen.LV (MiscParser.Location_reg (thread, register), value)) ->
     let register = parse_register register in
     let value = unwrap_val value in
-    let values = RegisterMap.add (thread, register) value values in
     (* Avoiding Map.update to retain compatibility with Ocaml < 4.06.0. *)
     (* TODO: Switch to update to save second traversal of map, if possible. *)
-    (* TODO: Check that register only gets added once? *)
-    let register_list = try
-      register :: ThreadMap.find thread registers
-    with
-    | Not_found -> [register]
-    in
-    let registers = ThreadMap.add thread register_list registers in
-    (values, registers)
+    let pairs = (register, value) :: try ThreadMap.find thread accumulator with Not_found -> [] in
+    ThreadMap.add thread pairs accumulator
   | _ -> failwith "not supported"
 
-(* Parse final condition and return two maps: *)
-(* - Unique register identification (thread number, register number) to expected value. *)
-(* - Thread number to registers found in that thread mentioned in the condition. *)
-let parse_condition (litmus : Lisa.litmus) : int RegisterMap.t * int list ThreadMap.t =
+(* Parse final condition and return a map from thread number to lists of registers and expected values. *)
+let parse_condition (litmus : Lisa.litmus) : (int * int) list ThreadMap.t =
   let _, _, condition, _ = litmus.Lisa.final in
 
   (* Check this is an exists expression and get the enclosed logic. *)
@@ -490,45 +561,7 @@ let parse_condition (litmus : Lisa.litmus) : int RegisterMap.t * int list Thread
   | _ -> failwith "not supported"
   in
 
-  parse_condition_expression (RegisterMap.empty, ThreadMap.empty) expression
-
-(* Return a thread with write instructions (to a dummy location) for a set of registers appended. *)
-let append_dummy_writes
-  (thread : BellBase.parsedPseudo list)
-  (registers : int list)
-: BellBase.parsedPseudo list =
-  let writes = List.map (fun register ->
-    let reg_or_imm = Regi (GPRreg register) in
-    let addr_op = Addr_op_atom(Abs(Symbolic dummy_global))in
-    Instruction(Pst(addr_op, reg_or_imm, []))) registers
-  in
-  thread @ writes
-
-(* Return litmus with extra write instructions appended. *)
-(* The extra writes are from registers mentioned in the constraint to a dummy location. *)
-(* The resulting write events are found later and used to indentify useful parts of the event structure. *)
-let add_dummy_constraint_writes
-  (litmus : Lisa.litmus)
-  (thread_to_registers : int list ThreadMap.t)
-: Lisa.litmus =
-  let program = List.map2 (fun thread_name thread ->
-    let registers =
-      try ThreadMap.find thread_name thread_to_registers
-      with Not_found -> [] in
-    append_dummy_writes thread registers) litmus.Lisa.threads litmus.Lisa.program
-  in
-  Lisa.{ litmus with program }
-
-(* Return the set of events caused by dummy write instructions that match the final condition. *)
-let get_must_execute (events : events) (expected : int RegisterMap.t) : EventStructure.set =
-  let writes = List.filter (fun write ->
-    match write.w_from with
-    | Some source -> 
-      write.w_into.global = dummy_global
-      && write.w_value = (RegisterMap.find source expected)
-    | None -> false) events.writes
-  in
-  List.map (fun write -> write.w_id) writes
+  parse_condition_expression ThreadMap.empty expression
 
 (* Translate a program AST into an event structure, this is the entrypoint into the module. *)
 (* `init` gives the initial values for global variables, letting the init event justify non-zero reads. *)
@@ -543,26 +576,40 @@ let translate litmus minimum maximum =
   (* This boxed counter is used to make sure all events get unique ID numbers. *)
   let next_id = ref (init_id + 1) in (* TODO: wrap in a function *)
 
-  let (register_to_value, thread_to_registers) = parse_condition litmus in
-  let litmus = add_dummy_constraint_writes litmus thread_to_registers in
+  let condition = parse_condition litmus in
 
   (* Translate each thread and compose the resulting event structures together. *)
-  let compose_threads (events : events) (thread_name : int) (instructions : BellBase.parsedPseudo list)
-  : events =
+  let compose_threads
+    ((events, accept) : events * EventStructure.set list)
+    (thread_name : int)
+    (instructions : BellBase.parsedPseudo list)
+  : events * EventStructure.set list =
     let instructions = Array.of_list instructions in
-    let subtree = translate_instructions thread_name instructions 0 Store.empty values next_id 0 in
-    product events subtree
+    let program_counter = 0 in
+    let depth = 0 in
+    let thread_condition = ThreadMap.find thread_name condition in
+    let subtree, subaccept = translate_instructions
+      thread_name
+      instructions
+      thread_condition
+      program_counter
+      Store.empty
+      values
+      next_id
+      init_id
+      depth
+    in
+    (product events subtree), subaccept :: accept
   in
-  let events = List.fold_left2 compose_threads empty_events litmus.Lisa.threads litmus.Lisa.program in
+  let events, accept = List.fold_left2
+    compose_threads (empty_events, []) litmus.Lisa.threads litmus.Lisa.program
+  in
 
   (* Add the order relations for the init event. *)
   let events = prefix_event events init_id in
 
   (* Generate the same location relation. *)
   let same_location = match_locations events.reads events.writes in
-
-  (* Build set of write events that match the end condition. *)
-  let must_execute = get_must_execute events register_to_value in
 
   (* Add virtual writes tied to init with all the values in the initialisation list. *)
   let events = {
@@ -586,4 +633,4 @@ let translate litmus minimum maximum =
     conflicts = events.conflict;
     order = events.order;
     sloc = same_location;
-  }, must_execute)
+  }, accept)
