b37dcbeac5df2eda0eaa069643a3f998ddd75a32
Radu Grigore <radugrigore@gmail.com>
2018-01-30 11:37:42 +0000

    hack

--- src/Models/CatRA.ml
+++ src/Models/CatRA.ml
@@ -15,6 +15,17 @@ let cat_constrain rf co po =
   let fr = fr rf co in
   let hb_id, hb = mk_fresh_reln () in
   let hb_refl_id, hb_refl = mk_fresh_reln () in
+  SoAny (hb_id, 2, And
+    [ rel_subset po_strict hb
+    ; rel_subset rf hb
+    ; transitive hb
+    ; irreflexive hb
+    ; SoAny (hb_refl_id, 2, And
+      [ rel_subset mk_eq hb_refl
+      ; rel_subset hb hb_refl
+      ; acyclic (sequence co hb_refl)
+    ; acyclic (sequence fr hb_refl) ] ) ] )
+  (*
   And
     [ SoAny (hb_id, 2,
       And
@@ -30,6 +41,7 @@ let cat_constrain rf co po =
         ; irreflexive (sequence co hb_refl)
         ; irreflexive (sequence fr hb_refl)
         (* note: no support for RMW *) ] ) ]
+*)
 
 let do_decide es accept =
   let size = es.E.events_number in
